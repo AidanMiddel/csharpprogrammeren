98e93fc84de6bb28cb1480ceeb4d523d590bece8
AidanMiddel <70641287+AidanMiddel@users.noreply.github.com>
2022-02-01 12:05:06 +0100

    cases added

--- zak van sinterklaas/zak van sinterklaas/Program.cs
+++ zak van sinterklaas/zak van sinterklaas/Program.cs
@@ -13,66 +13,133 @@ namespace dezakvansinterklaas
             int SkylanderAantal = 0;
             TheSkylanderBucket theSkylanderBucket = new TheSkylanderBucket();
 
-            Skylander FireBreaths = new FireBreath("Fire Breath ", "Spuw vuur op wat Chompies");
-            Skylander FireDashs = new FireDash("Fire Dash ", "Ga snel weg met de Fire Dash");
-            Skylander FireTeleports = new FireTeleport("Fire Teleport", "teleporteer snel weg van chompies\n");
+            
+            
+            
 
-            theSkylanderBucket.AddSkylander(FireBreaths);
-            theSkylanderBucket.AddSkylander(FireDashs);
-            theSkylanderBucket.AddSkylander(FireTeleports);
+            
+            
+            
 
 
-            foreach (Skylander skylander in theSkylanderBucket.GetSkylanders())
+            bool bshouldRun = true;
+            while (bshouldRun)
             {
-                Console.WriteLine(skylander.Skylandername + skylander.Skylanderdescription);
-                SkylanderAantal = theSkylanderBucket.GetSkylandersCount();
-            }
-
-            foreach (Skylander skylander in theSkylanderBucket.GetSkylanders())
-            {
-                Console.WriteLine($"Opties: {Index} {skylander.Skylandername}");
-                Index++;
-            }
-
-            while (true)
-            {
-                int deKeuze;
-                string Input = Console.ReadLine();
-                Int32.TryParse(Input, out deKeuze);
-                Console.Clear();
-                Index = 1;
-
-                if (!Int32.TryParse(Input, out deKeuze))
+                int inputNumber;
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out inputNumber))
                 {
-                    Console.WriteLine("invalide optie\n");
-                    foreach (Skylander skylander in theSkylanderBucket.GetSkylanders())
+                    switch (inputNumber)
                     {
-                        Console.WriteLine($"Opties: {Index} {skylander.Skylandername}");
-                        Index++;
+                        case 0:
+                            Console.WriteLine("Programma is zelfmoord aan het plegen");
+                            bshouldRun = false;
+                            break;
+
+                        case 1:
+                            Skylander FireBreaths = new FireBreath("Fire Breath ", "Spuw vuur op wat Chompies");
+                            theSkylanderBucket.AddSkylander(FireBreaths);
+                            Console.WriteLine($"Jij gebruikt Fire Breath op een chompy en deed 20 schade!");
+                            break;
+
+                        case 2:
+                            Skylander FireDashs = new FireDash("Fire Dash ", "Ga snel weg met de Fire Dash");
+                            theSkylanderBucket.AddSkylander(FireDashs);
+                            Console.WriteLine("Jij gebruikt Fire Dash om te vluchten!");
+                            break;
+
+                        case 3:
+                            Skylander FireTeleports = new FireTeleport("Fire Teleport", "teleporteer snel weg van chompies\n");
+                            theSkylanderBucket.AddSkylander(FireTeleports);
+                            Console.WriteLine("Jij gebruikt Fire Teleport om weg te teleporteren!");
+                            break;
+
+                        case 6:
+                            int amountOfFireBreath = 0;
+                            int amountOfFireDash = 0;
+                            int amountOfFireTeleport = 0;
+                            foreach (var item in theSkylanderBucket.GetSkylanders())
+                            {
+                                if (item is FireBreath)
+                                {
+                                    amountOfFireBreath++;
+                                }
+                                if (item is FireDash)
+                                {
+                                    amountOfFireDash++;
+                                }
+                                if (item is FireTeleport)
+                                {
+                                    amountOfFireTeleport++;
+                                }
+                            }
+                            Console.WriteLine($"Gebruikte fireBrearths: {amountOfFireBreath} Gebruikte Fire Dashes: {amountOfFireDash} Gebruikte Fire teleports: {amountOfFireTeleport}");
+                            //Console.WriteLine("heal value = {0}", userinput);
+                            break;
+
+                        default:
+                            Console.WriteLine("wrong number");
+                            bshouldRun = true;
+                            break;
                     }
                 }
 
-                if (Int32.TryParse(Input,out deKeuze) && deKeuze <= SkylanderAantal)
-                {
-                    theSkylanderBucket.Use(deKeuze);
-                    Console.WriteLine("\n");
-                    foreach (Skylander skylander in theSkylanderBucket.GetSkylanders())
-                    {
-                        Console.WriteLine($"Opties: {Index} {skylander.Skylandername}");
-                        Index++;
-                    }
-                }
-
-                if (deKeuze > SkylanderAantal)
-                {
-                    Console.WriteLine("invalide optie\n");
-                    foreach (Skylander skylander in theSkylanderBucket.GetSkylanders())
-                    {
-                        Console.WriteLine($"Opties: {Index} {skylander.Skylandername}");
-                        Index++;
-                    }
-                }
             }
+
+
+
+
+            //foreach (Skylander skylander in theSkylanderBucket.GetSkylanders())
+            //{
+            //Console.WriteLine(skylander.Skylandername + skylander.Skylanderdescription);
+            //SkylanderAantal = theSkylanderBucket.GetSkylandersCount();
+            //}
+
+            //foreach (Skylander skylander in theSkylanderBucket.GetSkylanders())
+            //{
+            //Console.WriteLine($"Opties: {Index} {skylander.Skylandername}");
+            //Index++;
+            //}
+
+            //while (true)
+            //int deKeuze;
+            //{
+            //string Input = Console.ReadLine();
+            //Int32.TryParse(Input, out deKeuze);
+            //Console.Clear();
+            //Index = 1;
+
+            //if (!Int32.TryParse(Input, out deKeuze))
+            //{
+            //Console.WriteLine("invalide optie\n");
+            //foreach (Skylander skylander in theSkylanderBucket.GetSkylanders())
+            //{
+            //Console.WriteLine($"Opties: {Index} {skylander.Skylandername}");
+            //Index++;
+            //}
+            //}
+
+            //if (Int32.TryParse(Input,out deKeuze) && deKeuze <= SkylanderAantal)
+            //{
+            //theSkylanderBucket.Use(deKeuze);
+            //Console.WriteLine("\n");
+            //foreach (Skylander skylander in theSkylanderBucket.GetSkylanders())
+            //{
+            //Console.WriteLine($"Opties: {Index} {skylander.Skylandername}");
+            //Index++;
+            //}
+            //}
+
+            //if (deKeuze > SkylanderAantal)
+            //{
+            //Console.WriteLine("invalide optie\n");
+            //foreach (Skylander skylander in theSkylanderBucket.GetSkylanders())
+            //{
+            //Console.WriteLine($"Opties: {Index} {skylander.Skylandername}");
+            //Index++;
+            //}
+            //}
+            //}
         }
     }
 }
